752dc9e52aff80a9075817be48b5cde7d11d3530
Saad Panjwani <srp@Saads-MacBook-Air.local>
2025-07-12 15:18:40 +0500

    🔧 Add UI components: button & input

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,15 +1,24 @@
-// components/ui/input.tsx
-import React from 'react';
+// app/components/ui/input.tsx
+import { InputHTMLAttributes, forwardRef } from "react"
+import { cn } from "app/lib/utils"
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input: React.FC<InputProps> = ({ ...props }) => {
-  return (
-    <input
-      {...props}
-      className={`border border-gray-300 rounded px-3 py-2 w-full ${props.className ?? ''}`}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
 
-export default Input;
+Input.displayName = "Input"
+
+export { Input }
